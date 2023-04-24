d800f24e179f699ea374d2c30f39dcfdc710cfc8
Akhirahstrong <codeclubdetroit@gmail.com>
2023-04-24 01:55:27 -0400

    Unit 3 Heating Cooling

---
+++ script.js
@@ -0,0 +1,13 @@
+let actualTemp = 56;
+let desiredTemp = 72;
+
+// actualTemp = 80;
+// desiredTemp = 72;
+
+if (actualTemp > desiredTemp) {
+  console.log("Run AC");
+} else if (actualTemp < desiredTemp) {
+  console.log("Run Heat");
+} else {
+  console.log("Standby");
+}
