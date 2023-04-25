94247c4b0a08f4900bbcf6bc8454fe0a5686567e
Akhirahstrong <codeclubdetroit@gmail.com>
2023-04-24 21:50:30 -0400

    Extended Challenge

--- script.js
+++ script.js
@@ -11,3 +11,20 @@ if (actualTemp > desiredTemp) {
 } else {
   console.log("Standby");
 }
+
+let tempCelsius = 25;
+let targetUnit = "K";
+
+switch (targetUnit) {
+  case "C":
+    console.log(tempCelsius + "°C");
+    break;
+  case "F":
+    console.log(tempCelsius * 1.8 + 32 + "°F");
+    break;
+  case "K":
+    console.log(tempCelsius + 237.15 + "K");
+    break;
+  default:
+    console.log("That is an invalid unit");
+}
